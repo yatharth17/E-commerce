df068560a3eeb6be0c747fac83be9fc1ec10ec05
yatharth17 <yatharthkatyal4@gmail.com>
2020-02-13 22:07:42 +0530

    Added routing using HOC withrouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css'
 import HomePage from './pages/homepage/homepage'
+import {Route, Switch} from 'react-router-dom'
+const HatsPage=()=>{
+  return(
+<div>
+    <h1>HATS PAGE</h1>
+  </div>
+  )
+  
+}
 function App() {
   return (
     <div >
-     <HomePage/>
+     <Switch>
+     <Route exact path='/' component={HomePage}/>
+     <Route path='/hats' component={HatsPage}/> 
+     </Switch>
+    
+     
     </div>
   );
 }
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import Directory from '../../components/directory/directory'
 import './homepage.scss';
 
-const HomePage = () => (
-  <div className='homepage'>
+const HomePage = () => {
+  return(
+    <div className='homepage'>
     <Directory />
   </div>
-);
+  )
+
+  }
 
 
 export default HomePage;
