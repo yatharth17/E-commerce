8ec15d7981dcf0af086fc491330594b0715dd829
yatharth17 <yatharthkatyal4@gmail.com>
2020-03-15 09:29:09 +0530

    shop-page

---
+++ src/components/preview-collection/preview-collection.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import CollectionItem from '../collection-item/collection-item'
+import './preview-collection.scss'
+
+const CollectionPreview=({title,items})=>{
+    return(
+        <div className='collection-preview'>
+            <h1>{title.toUpperCase()}</h1>
+            <div className='preview'>
+                {
+                    items.filter((item,idx)=>idx<4).map(({id,...otherItemProps})=>(<CollectionItem key={id} {...otherItemProps}/>))
+                }
+            </div>
+        </div>
+    )
+}
+export default CollectionPreview
--- src/pages/shop/shoppage.jsx
+++ src/pages/shop/shoppage.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import SHOP_DATA from './shop_data'
+
+import CollectionPreview from '../../components/preview-collection/preview-collection';
 class ShopPage extends Component {
-    state = { SHOP_DATA }
+    state = { 
+        collections:SHOP_DATA }
     render() { 
-        return (
-            <div>
-                ShopPage
-            </div>
-          );
+        const {collections}=this.state
+        return(
+            <div className='shop-page'>{
+              collections.map(({id,...otherCollectionProps})=>(
+                  <CollectionPreview key={id} {...otherCollectionProps}/>
+              ))
+            }</div>
+        )
+        
     }
 }
  
