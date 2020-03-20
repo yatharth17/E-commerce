b2f57907867501d86c5008eb388f311b5cceff2d
yatharth17 <yatharthkatyal4@gmail.com>
2020-03-20 21:13:55 +0530

    added_redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import App from './App';
-
+import {Provider} from 'react-redux'
 
 ReactDOM.render(
+   <Provider>
 <BrowserRouter>
 <App />
 </BrowserRouter>
+   </Provider> 
+
 , document.getElementById('root'));
 
 
