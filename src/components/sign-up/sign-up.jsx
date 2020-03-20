223594e82f5785ccbe5cf63c5b6aa209b6c85de1
yatharth17 <yatharthkatyal4@gmail.com>
2020-03-20 10:48:56 +0530

    updated signup

--- src/components/sign-up/sign-up.jsx
+++ src/components/sign-up/sign-up.jsx
@@ -3,7 +3,7 @@ import FormInput from '../form-input/form-input'
 import {auth,createUserProfileDocument} from '../../firebase/firebase.utils'
 import CustomButton from '../custom-button/custom-button'
 import './sign-up.scss'
-import FormInput from '../form-input/form-input';
+
 
 class SignUp extends Component{
     state={
@@ -51,7 +51,7 @@ class SignUp extends Component{
                       onChange={this.handleChange}
                       label="Display Name"
                       required
-                    ></FormInput>
+                    />
                     <FormInput
                       type='email'
                       name='email'
@@ -59,7 +59,7 @@ class SignUp extends Component{
                       onChange={this.handleChange}
                       label="email"
                       required
-                    ></FormInput>
+                    />
                     <FormInput
                       type='password'
                       name='password'
@@ -67,7 +67,7 @@ class SignUp extends Component{
                       onChange={this.handleChange}
                       label="password"
                       required
-                    ></FormInput>
+                    />
                     <FormInput
                       type='password'
                       name='confirmPassword'
@@ -75,7 +75,7 @@ class SignUp extends Component{
                       onChange={this.handleChange}
                       label="confirmPassword"
                       required
-                    ></FormInput>
+                    />
                     <CustomButton type='submit'>SIGN UP</CustomButton>
                 </form>
             </div>
--- src/pages/sign-insign-up/sign-in-sign-up.jsx
+++ src/pages/sign-insign-up/sign-in-sign-up.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in'
 import './sign-in-sign-up.scss'
-
+import SignUp from '../../components/sign-up/sign-up'
 const SignInSignUpPage=()=>(
     <div className='sign-in-and-sign-up'>
-     <SignIn></SignIn>
+     <SignIn/>
+     <SignUp/>
     </div>
 )
 export default SignInSignUpPage
