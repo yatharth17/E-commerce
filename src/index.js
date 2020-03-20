89970bb628771279781a91339e798c5d25402a60
yatharth17 <yatharthkatyal4@gmail.com>
2020-03-20 21:42:55 +0530

    added_action

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import App from './App';
 import {Provider} from 'react-redux'
-
+import store from './redux/store'
 ReactDOM.render(
-   <Provider>
+   <Provider store={store}>
 <BrowserRouter>
 <App />
 </BrowserRouter>
